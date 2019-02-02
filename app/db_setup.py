afd38b6717da569c5b68821f3e79df653f0f0a87
Vineet Rao <28603906+VinWare@users.noreply.github.com>
2019-02-02 18:05:20 +0530

    DB Setup done

--- app/db_setup.py
+++ app/db_setup.py
@@ -2,11 +2,11 @@ from app import app
 from flask_mysqldb import MySQL
 import json
 
-mysql = MySQL(app)
-
 with open('private/config.json') as file:
     config = json.load(file)
 app.config['MYSQL_HOST'] = config['db']['host']
 app.config['MYSQL_USER'] = config['db']['user']
 app.config['MYSQL_PASSWORD'] = config['db']['password']
 app.config['MYSQL_DB'] = config['db']['dbname']
+
+mysql = MySQL(app)
--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,12 @@
 from app import app
 from flask import jsonify
 from .auth_setup import auth
+from .db_setup import mysql
 
 @app.route('/')
 @app.route('/index')
 @auth.login_required
 def index():
-    return jsonify({'body': 'Hello, World!'})
+    cur = mysql.connection.cursor()
+    cur.execute('SELECT * FROM user')
+    return jsonify(cur.fetchall())
