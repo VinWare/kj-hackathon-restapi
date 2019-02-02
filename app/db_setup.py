d71c2600e622f0295de3965213205dfb29566b99
Vineet Rao <28603906+VinWare@users.noreply.github.com>
2019-02-02 17:48:53 +0530

    DB setup basic

---
+++ app/db_setup.py
@@ -0,0 +1,12 @@
+from app import app
+from flask_mysqldb import MySQL
+import json
+
+mysql = MySQL(app)
+
+with open('private/config.json') as file:
+    config = json.load(file)
+app.config['MYSQL_HOST'] = config['db']['host']
+app.config['MYSQL_USER'] = config['db']['user']
+app.config['MYSQL_PASSWORD'] = config['db']['password']
+app.config['MYSQL_DB'] = config['db']['dbname']
