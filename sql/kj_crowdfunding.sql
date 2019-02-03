8d45a0c8bb17336894a6616f93b076229b28c87e
Vineet Rao <28603906+VinWare@users.noreply.github.com>
2019-02-03 07:45:06 +0530

    Modified DB

--- sql/kj_crowdfunding.sql
+++ sql/kj_crowdfunding.sql
@@ -1,226 +1,243 @@
--- phpMyAdmin SQL Dump
--- version 4.7.4
--- https://www.phpmyadmin.net/
+-- MySQL dump 10.13  Distrib 8.0.15, for Linux (x86_64)
 --
--- Host: 127.0.0.1
--- Generation Time: Feb 02, 2019 at 08:59 PM
--- Server version: 10.1.30-MariaDB
--- PHP Version: 7.2.1
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
-
+-- Host: localhost    Database: kj_crowdfunding
+-- ------------------------------------------------------
+-- Server version	8.0.15
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `kj_crowdfunding`
---
-
--- --------------------------------------------------------
+ SET NAMES utf8mb4 ;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
 -- Table structure for table `donations`
 --
 
+DROP TABLE IF EXISTS `donations`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
 CREATE TABLE `donations` (
   `sponsor_id` int(11) NOT NULL,
-  `student_id` int(11) NOT NULL
+  `student_id` int(11) NOT NULL,
+  PRIMARY KEY (`sponsor_id`,`student_id`),
+  KEY `sponsor_id` (`sponsor_id`),
+  KEY `student_id` (`student_id`),
+  CONSTRAINT `donations_ibfk_1` FOREIGN KEY (`sponsor_id`) REFERENCES `sponsor` (`user_id`),
+  CONSTRAINT `donations_ibfk_2` FOREIGN KEY (`student_id`) REFERENCES `student` (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- --------------------------------------------------------
+--
+-- Dumping data for table `donations`
+--
+
+LOCK TABLES `donations` WRITE;
+/*!40000 ALTER TABLE `donations` DISABLE KEYS */;
+/*!40000 ALTER TABLE `donations` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `internships`
+--
+
+DROP TABLE IF EXISTS `internships`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `internships` (
+  `sponsor_id` int(11) DEFAULT NULL,
+  `internship_id` int(11) DEFAULT NULL,
+  `internship_name` varchar(255) DEFAULT NULL,
+  `internship_description` text
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `internships`
+--
+
+LOCK TABLES `internships` WRITE;
+/*!40000 ALTER TABLE `internships` DISABLE KEYS */;
+/*!40000 ALTER TABLE `internships` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `invest`
 --
 
+DROP TABLE IF EXISTS `invest`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
 CREATE TABLE `invest` (
   `user_id` int(11) DEFAULT NULL,
   `project_id` int(11) DEFAULT NULL,
-  `amount` decimal(10,0) NOT NULL
+  `amount` decimal(10,0) NOT NULL,
+  KEY `sponsor_invest` (`user_id`),
+  KEY `project_invest` (`project_id`),
+  CONSTRAINT `project_invest` FOREIGN KEY (`project_id`) REFERENCES `project` (`project_id`) ON DELETE SET NULL ON UPDATE SET NULL,
+  CONSTRAINT `sponsor_invest` FOREIGN KEY (`user_id`) REFERENCES `sponsor` (`user_id`) ON DELETE SET NULL ON UPDATE SET NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- --------------------------------------------------------
+--
+-- Dumping data for table `invest`
+--
+
+LOCK TABLES `invest` WRITE;
+/*!40000 ALTER TABLE `invest` DISABLE KEYS */;
+/*!40000 ALTER TABLE `invest` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `project`
 --
 
+DROP TABLE IF EXISTS `project`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
 CREATE TABLE `project` (
-  `project_id` int(11) NOT NULL,
+  `project_id` int(11) NOT NULL AUTO_INCREMENT,
   `project_name` varchar(255) NOT NULL,
   `category` enum('IT Solutions','Security') NOT NULL,
   `phase` int(11) NOT NULL,
   `cost` decimal(10,0) NOT NULL,
   `start_date` date NOT NULL,
   `complete_date` date NOT NULL,
-  `project_type` enum('Product','Service') NOT NULL
+  `project_type` enum('Product','Event') DEFAULT NULL,
+  PRIMARY KEY (`project_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- --------------------------------------------------------
+--
+-- Dumping data for table `project`
+--
+
+LOCK TABLES `project` WRITE;
+/*!40000 ALTER TABLE `project` DISABLE KEYS */;
+/*!40000 ALTER TABLE `project` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `project_members`
 --
 
+DROP TABLE IF EXISTS `project_members`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
 CREATE TABLE `project_members` (
   `user_id` int(11) NOT NULL,
-  `project_id` int(11) NOT NULL
+  `project_id` int(11) NOT NULL,
+  PRIMARY KEY (`user_id`,`project_id`),
+  KEY `user_id` (`user_id`),
+  KEY `project_id` (`project_id`),
+  KEY `user_id_2` (`user_id`),
+  KEY `project_id_2` (`project_id`),
+  CONSTRAINT `project_members_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `student` (`user_id`),
+  CONSTRAINT `project_members_ibfk_2` FOREIGN KEY (`project_id`) REFERENCES `project` (`project_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- --------------------------------------------------------
+--
+-- Dumping data for table `project_members`
+--
+
+LOCK TABLES `project_members` WRITE;
+/*!40000 ALTER TABLE `project_members` DISABLE KEYS */;
+/*!40000 ALTER TABLE `project_members` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `sponsor`
 --
 
+DROP TABLE IF EXISTS `sponsor`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
 CREATE TABLE `sponsor` (
   `user_id` int(11) NOT NULL,
-  `company` varchar(255) NOT NULL
+  `company` varchar(255) NOT NULL,
+  KEY `user_sponsor` (`user_id`),
+  CONSTRAINT `user_sponsor` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- --------------------------------------------------------
+--
+-- Dumping data for table `sponsor`
+--
+
+LOCK TABLES `sponsor` WRITE;
+/*!40000 ALTER TABLE `sponsor` DISABLE KEYS */;
+/*!40000 ALTER TABLE `sponsor` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `student`
 --
 
+DROP TABLE IF EXISTS `student`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
 CREATE TABLE `student` (
   `user_id` int(11) NOT NULL,
   `college_name` varchar(255) NOT NULL,
   `year` int(11) NOT NULL,
-  `branch` varchar(31) NOT NULL
+  `branch` varchar(31) NOT NULL,
+  KEY `user_student` (`user_id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `user_student` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- --------------------------------------------------------
+--
+-- Dumping data for table `student`
+--
+
+LOCK TABLES `student` WRITE;
+/*!40000 ALTER TABLE `student` DISABLE KEYS */;
+/*!40000 ALTER TABLE `student` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `user`
 --
 
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
 CREATE TABLE `user` (
-  `user_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
   `user_name` varchar(255) NOT NULL,
   `full_name` varchar(127) NOT NULL,
   `password_hash` varchar(128) NOT NULL,
-  `blockchain` varchar(63) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `blockchain` varchar(63) NOT NULL,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `user_name` (`user_name`),
+  KEY `user_id` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Indexes for dumped tables
+-- Dumping data for table `user`
 --
 
---
--- Indexes for table `donations`
---
-ALTER TABLE `donations`
-  ADD PRIMARY KEY (`sponsor_id`,`student_id`),
-  ADD KEY `sponsor_id` (`sponsor_id`),
-  ADD KEY `student_id` (`student_id`);
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,'vineet','Vineet Rao','pbkdf2:sha256:50000$Q0QYDWSO$3e0f3111a85e68368972cac8f0bb62911b688dfe335c2df5261dbed38902fff6','ABCD'),(3,'abcd','Vineet Rao','pbkdf2:sha256:50000$FugfVIZ7$3814b2dd03a297efc229936a80b36b3cbf7db51e4e607c377ce610e59da0fa79','ABCD');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
---
--- Indexes for table `invest`
---
-ALTER TABLE `invest`
-  ADD KEY `sponsor_invest` (`user_id`),
-  ADD KEY `project_invest` (`project_id`);
-
---
--- Indexes for table `project`
---
-ALTER TABLE `project`
-  ADD PRIMARY KEY (`project_id`);
-
---
--- Indexes for table `project_members`
---
-ALTER TABLE `project_members`
-  ADD PRIMARY KEY (`user_id`,`project_id`),
-  ADD KEY `user_id` (`user_id`),
-  ADD KEY `project_id` (`project_id`),
-  ADD KEY `user_id_2` (`user_id`),
-  ADD KEY `project_id_2` (`project_id`);
-
---
--- Indexes for table `sponsor`
---
-ALTER TABLE `sponsor`
-  ADD KEY `user_sponsor` (`user_id`);
-
---
--- Indexes for table `student`
---
-ALTER TABLE `student`
-  ADD KEY `user_student` (`user_id`),
-  ADD KEY `user_id` (`user_id`);
-
---
--- Indexes for table `user`
---
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`user_id`),
-  ADD UNIQUE KEY `user_name` (`user_name`),
-  ADD KEY `user_id` (`user_id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `project`
---
-ALTER TABLE `project`
-  MODIFY `project_id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `user`
---
-ALTER TABLE `user`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `donations`
---
-ALTER TABLE `donations`
-  ADD CONSTRAINT `donations_ibfk_1` FOREIGN KEY (`sponsor_id`) REFERENCES `sponsor` (`user_id`),
-  ADD CONSTRAINT `donations_ibfk_2` FOREIGN KEY (`student_id`) REFERENCES `student` (`user_id`);
-
---
--- Constraints for table `invest`
---
-ALTER TABLE `invest`
-  ADD CONSTRAINT `project_invest` FOREIGN KEY (`project_id`) REFERENCES `project` (`project_id`) ON DELETE SET NULL ON UPDATE SET NULL,
-  ADD CONSTRAINT `sponsor_invest` FOREIGN KEY (`user_id`) REFERENCES `sponsor` (`user_id`) ON DELETE SET NULL ON UPDATE SET NULL;
-
---
--- Constraints for table `project_members`
---
-ALTER TABLE `project_members`
-  ADD CONSTRAINT `project_members_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `student` (`user_id`),
-  ADD CONSTRAINT `project_members_ibfk_2` FOREIGN KEY (`project_id`) REFERENCES `project` (`project_id`);
-
---
--- Constraints for table `sponsor`
---
-ALTER TABLE `sponsor`
-  ADD CONSTRAINT `user_sponsor` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `student`
---
-ALTER TABLE `student`
-  ADD CONSTRAINT `user_student` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
-COMMIT;
-
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-02-03  7:44:04
